9fd0cbfd2ce03efb7dab93e8ede1970c85305864
John Napiorkowski <johnn@John-Napiorkowski-MacBook-Pro.local>
2010-07-27 10:15:33 -0400

    more playing with weakn

--- lib/MyMethodTrait.pm
+++ lib/MyMethodTrait.pm
@@ -4,20 +4,19 @@ role MyMethodTrait {
     has tag => (is=>'ro', isa=>'Str', required=>1, default=>'div');
     around wrap(ClassName $class: $code, %options) {
         my $method_obj;
-        Scalar::Util::weaken(my $weakened_method_obj = $method_obj);
+        my $weakened_method_obj = \$method_obj;
         my $replacement = sub {
             my ($self, $string) = @_;
-
-warn ref $self; ## Good
-warn $string; ## Good
-warn ref $method_obj;  ## Is the correct objecy
-warn ref $weakened_method_obj;  ## is undef no matter what I do
-
-            my $tag = $weakened_method_obj->tag;
             my $title = $self->$code($string);
+            my $tag = ${$weakened_method_obj}->tag;
+            Scalar::Util::weaken $weakened_method_obj;
             return "<$tag>$title</$tag>";
         };
         $method_obj = $class->$orig($replacement, %options);
         return $method_obj;
     }
 }
+
+__END__
+
+
