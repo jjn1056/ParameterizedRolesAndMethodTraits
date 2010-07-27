7b8bbe6cfe908d58177f4b19d878d0bc0954f73b
John Napiorkowski <johnn@new-host-3.home>
2010-07-26 20:18:58 -0400

    start to weaken possible leak

--- lib/MyMethodTrait.pm
+++ lib/MyMethodTrait.pm
@@ -1,9 +1,11 @@
 use MooseX::Declare;
 role MyMethodTrait {
+    use Scalar::Util;
     has tag => (is=>'ro', isa=>'Str', required=>1, default=>'div');
     around wrap(ClassName $class: $code, %options) {
         my $method_obj;
         my $new_method = method(Str $string) {
+            Scalar::Util::weaken($method_obj);
             my $tag = $method_obj->tag;
             my $title = $self->$code($string);
             return "<$tag>$title</$tag>";
