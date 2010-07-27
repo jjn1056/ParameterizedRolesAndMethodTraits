afdea4993ca27a4d3b2e3443083c886e188dad31
John Napiorkowski <johnn@John-Napiorkowski-MacBook-Pro.local>
2010-07-27 09:22:19 -0400

    trying to kill the circular ref

--- lib/MyMethodTrait.pm
+++ lib/MyMethodTrait.pm
@@ -4,13 +4,20 @@ role MyMethodTrait {
     has tag => (is=>'ro', isa=>'Str', required=>1, default=>'div');
     around wrap(ClassName $class: $code, %options) {
         my $method_obj;
-        my $new_method = method(Str $string) {
-            Scalar::Util::weaken($method_obj);
-            my $tag = $method_obj->tag;
+        Scalar::Util::weaken(my $weakened_method_obj = $method_obj);
+        my $replacement = sub {
+            my ($self, $string) = @_;
+
+warn ref $self; ## Good
+warn $string; ## Good
+warn ref $method_obj;  ## Is the correct objecy
+warn ref $weakened_method_obj;  ## is undef no matter what I do
+
+            my $tag = $weakened_method_obj->tag;
             my $title = $self->$code($string);
             return "<$tag>$title</$tag>";
         };
-        $method_obj = $class->$orig($new_method->actual_body, %options);
+        $method_obj = $class->$orig($replacement, %options);
         return $method_obj;
     }
 }
